c4079ad71de498839ed6dc68b502cc6a783845c4
落雨彡逸尘 <70144452+yichen-ender@users.noreply.github.com>
2026-08-08 16:39:03 +0800

    Revert src/endstone_arc_inventory/__init__.py to upstream

--- src/endstone_arc_inventory/__init__.py
+++ src/endstone_arc_inventory/__init__.py
@@ -1,6 +1,4 @@
 from endstone_arc_inventory.arc_inventory_plugin import ARCInventoryPlugin
 from endstone_arc_inventory.InventoryManager import InventoryManager
-from endstone_arc_inventory.safe import SafeManager
-from endstone_arc_inventory.ui import UIManager
 
-__all__ = ["ARCInventoryPlugin", "InventoryManager", "SafeManager", "UIManager"]
+__all__ = ["ARCInventoryPlugin", "InventoryManager"]
